dadd750189173eb6f10be78fa177410a7cdce7e9
uuhnaut69 <uuhnaut96@gmail.com>
2023-03-10 21:04:02 +0700

    feat: Init some services

---
+++ authentication/src/test/java/com/uuhnaut69/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uuhnaut69.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
